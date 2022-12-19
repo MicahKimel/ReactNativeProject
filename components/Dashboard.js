394c71c3e9df5577e64c174236c310e9c04004f7
Micah Kimel <micah.kimel@my.wheaton.edu>
2022-12-19 10:36:41 -0500

    graphing test

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,9 @@
 import TitleBar from "./TitleBar";
-import { NativeRouter, Route, Link, withRouter } from "react-router-native";
+import { SelectList } from 'react-native-dropdown-select-list'
+import React from "react";
+import {
+    LineChart
+  } from "react-native-chart-kit";
 import {
     SafeAreaView,
     ScrollView,
@@ -10,19 +14,103 @@ import {
     Button,
     useColorScheme,
     View,
+    Dimensions,
   } from 'react-native';
 const Dashboard = ({ navigation }) => {
+
+    const [selected, setSelected] = React.useState("");
+  
+    const data = [
+        {key:'1', value:'Mobiles'},
+        {key:'2', value:'Appliances'},
+        {key:'3', value:'Cameras'},
+        {key:'4', value:'Computers'},
+        {key:'5', value:'Vegetables'},
+        {key:'6', value:'Diary Products'},
+        {key:'7', value:'Drinks'},
+    ]
 
     Back = async event => {
         navigation.navigate("Login");
     }
-
-
     return (
         <View>
             <Text style={styles.sectionTitle}>Dashboard</Text>            
             <View style={styles.backbutton} >
                 <Button title="<" color="#202124" onPress={this.Back} />
+            </View>
+            <View>
+                <SelectList 
+                    setSelected={(val) => setSelected(val)} 
+                    data={data} 
+                    save="value"
+                />
+                <View>
+  <Text>Bezier Line Chart</Text>
+  <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        },
+        {
+            data: [
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100
+            ]
+          },
+          {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
+</View>
             </View>
         </View>
     );
